141980641a97a57150c968249c877709d309e890
shikumiru <code.0signal@gmail.com>
2023-12-13 21:16:04 +0900

    リスト

--- list.py
+++ list.py
@@ -1,5 +1,10 @@
-some_list = []
-if some_list:
-    print("There's something in here")
-else:
-    print("Hey, it's empty!")
+number_list = [number -1 for number in range(1,6)]
+print(number_list)
+
+# 奇数リスト
+a_list = [number for number in range(1, 6) if number % 2 == 1]
+print(a_list)
+
+# 偶数リスト
+b_list = [number for number in range(10) if number % 2 == 0]
+print(b_list)
