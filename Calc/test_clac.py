49d85c705f721473c0b4b7e3697ac4551914ffc4
shikumiru <code.0signal@gmail.com>
2024-03-24 16:29:25 +0900

    calc

---
+++ Calc/test_clac.py
@@ -0,0 +1,18 @@
+import unittest
+from calc import Calc
+
+class ClacTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(Calc().add(1, 2), 3)
+        
+    def test_minus(self):
+        self.assertEqual(Calc().minus(3,2),1)
+        
+    def test_multiply(self):
+        self.assertEqual(Calc.multiply(1, 2), 2)
+        
+    def test_divide(self):
+        self.assertEqual(Calc().divide(8, 2), 4)
+        
+if __name__ == "__main__":
+    unittest.main()
