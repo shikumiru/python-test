424dfdb4966be63468894f5c1aca7d082280d46b
shikumiru <code.0signal@gmail.com>
2024-03-29 15:54:21 +0900

    another_pattern

---
+++ selenium_prac/test_sample4-2.py
@@ -0,0 +1,19 @@
+import time
+from selenium.webdriver.common.by import By
+
+def test_check_google_translate_work(driver):
+    driver.get("https://translate.google.com/")
+    
+    time.sleep(3)
+    
+    input_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea")
+    input_text.send_keys("japan")
+    
+    time.sleep(3)
+    
+    output_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[1]/span[1]/span/span")
+    assert output_text.text == "日本"
+    return
+
+if __name__=="__main__":
+    test_check_google_translate_work()
