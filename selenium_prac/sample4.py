1b699ff3fd859a8c5865b68a334f8a5916897dc2
shikumiru <code.0signal@gmail.com>
2024-03-29 16:15:30 +0900

    fix

--- selenium_prac/sample4.py
+++ selenium_prac/sample4.py
@@ -11,7 +11,7 @@ def test_check_google_translate_work_properly():
     
     time.sleep(3)
     
-    input_search = driver.find_element(By.XPATH, "//*[@id='APjFqb']")
+    input_search = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
     input_search.clear()
     input_search.send_keys('翻訳')
     input_search.submit()
@@ -23,13 +23,13 @@ def test_check_google_translate_work_properly():
     
     time.sleep(3)
     
-    input_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea")
+    input_text = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
     input_text.send_keys("america")
     input_text.send_keys(Keys.ENTER)
     
     time.sleep(3)
     
-    output_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[1]/span[1]/span/span")
+    output_text = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[1]/span[1]/span/span')
     assert output_text.text == 'アメリカ'
     
     driver.quit()
--- selenium_prac/test_sample4-2.py
+++ selenium_prac/test_sample4-2.py
@@ -6,12 +6,12 @@ def test_check_google_translate_work(driver):
     
     time.sleep(3)
     
-    input_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea")
+    input_text = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
     input_text.send_keys("japan")
     
     time.sleep(3)
     
-    output_text = driver.find_element(By.XPATH, "//*[@id='yDmH0d']/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[1]/span[1]/span/span")
+    output_text = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[1]/span[1]/span/span')
     assert output_text.text == "日本"
     return
 
